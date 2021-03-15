7b4b121bc1640878604076f7642d5326e880665f
dylan1496 <dachaconp@unal.edu.co>
2021-03-15 12:28:37 -0500

    home

--- src/components/contentHome.jsx
+++ src/components/contentHome.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+const Home = styled.div`
+    padding-top: 4rem;
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+    @media (min-width: 768px) {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr) ;
+        column-gap: 2rem;
+    }
+    p {
+        line-height: 2;
+        
+    }
+`;
 
 const ContentHome = () => {
     const data = useStaticQuery(graphql`
@@ -7,24 +26,29 @@ const ContentHome = () => {
             allDatoCmsPagina(filter: { slug: { eq: "inicio" } } ) {
                 nodes {
                     titulo
-            contenido
-            imagen {
-                fluid {
-                    ...GatsbyDatoCmsFluid
+                    contenido
+                    imagen {
+                        fixed {
+                            ...GatsbyDatoCmsFixed
+                        }
+                    }
                 }
             }
-            }
-        }
         }
     `);
     const { titulo, contenido, imagen } = data.allDatoCmsPagina.nodes[0];
+    console.log(imagen);
     return (
         <>
-            <h2>{titulo}</h2>
-            <div>
+            <h2 css={css`
+                text-align: center;
+                font-size: 4rem;
+                margin-top: 4rem;
+            `}>{titulo}</h2>
+            <Home>
                 <p>{contenido}</p>
-                <img src={imagen.fluid.src} />
-            </div>
+                <img src={imagen.fixed.src} alt="Nuestro hotel" />
+            </Home>
         </>
     );
 }
